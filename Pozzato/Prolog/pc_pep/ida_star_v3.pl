a08520e4e1eb8c161fd1b22b3860741e7af28225
Giuseppe Biondi <giuv360@hotmail.it>
2020-11-06 17:49:10 +0100

    commenti

--- Pozzato/Prolog/pc_pep/ida_star_v3.pl
+++ Pozzato/Prolog/pc_pep/ida_star_v3.pl
@@ -12,31 +12,36 @@ ida_star(Soluzione):-
 	asserta(minimo(0)), % utilizzo asserta/retract per salvarmi la soglia come clausola
 	ida_aux(nodo(Start,[]),Soluzione).
 
+/*  */
 ida_aux(Nodo,Res):-
-	minimo(OldMin),
+	minimo(OldMin),		% leggo l'attuale minimo salvato
 	generaFigli(Nodo,[],Figli),
 	retract(minimo(OldMin)),%write("riparto da 0 con min: "),write_ln(OldMin),
-	asserta(minimo(0)),
+	asserta(minimo(0)),	% risetto minimo a 0 ??? quando rinizia ??
 	ida_aux2(Nodo,Figli,OldMin,[Nodo],Res).
 
+/* ricerco in profondita e se fallisce riparto da sopra */
 ida_aux2(_,Figli,OldMin,Espansi,Res):-  % non si può mettere in una riga sola con ; perchè si perderebbe il cut che evita backtracking
 	ida_aux3(Figli,OldMin,Espansi,Res),!.
 ida_aux2(Nodo,_,_,_,Res):-
 	ida_aux(Nodo,Res),!.		%DOMANDA: perchèquesta riga causa casi di backtracking con altre soluzioni senza cut?
 
+/* ricerca in profondità
+	
+*/
 %ida_aux3([],_,_,_):- !,fail.   % DOMANDA: perchè questa riga causa casi di backtracking con altre soluzioni?
 ida_aux3([nodo(N,Az)|_],_,_,Az):-finale(N),!.		%se è nodo finale
-ida_aux3([nodo(N,Az)|Figli],PesoSoglia,Espansi,Res):-					%se il nodo espanso ha costo F <PesoSoglia allora espando figli
+ida_aux3([nodo(N,Az)|Figli],PesoSoglia,Espansi,Res):-	%se il nodo espanso ha costo F <PesoSoglia allora espando figli
 	calcola_F(nodo(N,Az),F),
 	F =< PesoSoglia,!,
 	generaFigli(nodo(N,Az),Espansi,FigliDiN),%write("genero i figli di "+ N + " "),write_ln(FigliDiN),
 	append(FigliDiN,Figli,NuoviFigli),
 	append(FigliDiN,Espansi,NuoviEspansi),
 	ida_aux3(NuoviFigli,PesoSoglia,NuoviEspansi,Res).
-ida_aux3([nodo(N,Az)|Figli],PesoSoglia,Espansi,Res):-					%se il nodo espanso ha costo > PesoSoglia, non lo espando e salvo il suo costo
+ida_aux3([nodo(N,Az)|Figli],PesoSoglia,Espansi,Res):-	%se il nodo espanso ha costo > PesoSoglia, non lo espando e aggiorno soglia
 	minimo(NewPesoSoglia),
 	calcola_F(nodo(N,Az),F),
-	(F < NewPesoSoglia ; NewPesoSoglia == 0),!,	
+	(F < NewPesoSoglia ; NewPesoSoglia == 0),!,	 % se il consto F è minore dell'attuale soglia salvo F come soglia
 	retract(minimo(NewPesoSoglia)),
 	asserta(minimo(F)),%write("nuovo minimo: "),write(F + " "),writeln(N),
 	ida_aux3(Figli,PesoSoglia,Espansi,Res).		% invece di usare una lista di valori tengo traccia solo del PesoSoglia
