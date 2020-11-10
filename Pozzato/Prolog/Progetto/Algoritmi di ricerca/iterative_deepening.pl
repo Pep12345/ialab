9a3479a9fbe1b7830a83074ad3354d9497efdac9
Giuseppe Biondi <giuv360@hotmail.it>
2020-11-10 18:41:28 +0100

    commenti iterative deep

--- Pozzato/Prolog/Progetto/Algoritmi di ricerca/iterative_deepening.pl
+++ Pozzato/Prolog/Progetto/Algoritmi di ricerca/iterative_deepening.pl
@@ -1,14 +1,19 @@
-%aggiungere soglia limite
-% nota: trovare un modo per farlo finire quando non c'è la soluzione
 iterative_deepening(Soluzione):-
   iterative_deepening_aux(Soluzione,1).
 
 iterative_deepening_aux(Soluzione,Soglia):-
   depth_limit_search(Soluzione,Soglia),!.	% questo cut serve per cercare soluzioni alternative senza aumentare soglia
 iterative_deepening_aux(Soluzione,Soglia):-
+  num_colonne(NumCol),
+  num_righe(NumRighe),
   NuovaSoglia is Soglia+1,
+  NuovaSoglia < (log(NumCol * NumRighe) * 6), % DOMANDA: come calcolare l'upper bound
   iterative_deepening_aux(Soluzione,NuovaSoglia).
 
+/* prove soglie:	
+	- NumCol * NumRighe / N ... N = 2,3,4...8 ma non ci piaceva perchè dava soglie troppo grandi per labirinti grandi
+	- log(NumCol * NumRighe) * N) ... N = 4,5...8 forse troppo corta
+*/
 depth_limit_search(Soluzione,Soglia):-
   iniziale(S),
   dfs_aux(S,Soluzione,[S],Soglia).
